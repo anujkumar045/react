598bd4e68e991232dd496f5739486e1dbba6ad97
anujkumar045 <prasadanujkumar045@gmail.com>
2026-02-06 17:24:55 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Button from 'react-bootstrap/Button';
-
-const App=()=>{
+const App = () => {
   return (
     <>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <Button variant="primary">Primary</Button><br />
-      <Button variant="secondary">Secondary</Button><br />
-      <Button variant="success">Success</Button><br />
-      <Button variant="warning">Warning</Button><br />
-      <Button variant="danger">Danger</Button><br />
-      <Button variant="info">Info</Button><br />
-      <Button variant="light">Light</Button><br />
-      <Button variant="dark">Dark</Button><br /><hr />
-      <Button variant="link">Link</Button><br />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  );
+  )
 }
-
 export default App;
--- src/Bhopal.jsx
+++
@@ -1,9 +0,0 @@
-
-const Bhopal=()=>{
-    return(
-        <>
-        <h1>We live in Bhopal</h1>
-        </>
-    )
-}
-export default Bhopal;
